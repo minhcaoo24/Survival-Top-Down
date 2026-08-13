f41cb3b1ba99d5e2d2b9f38b14ecdc671bdaed35
minhcaoo24 <caonhatminh.work@gmail.com>
2026-08-14 01:41:53 +0700

    feat: player configuration

---
+++ Assets/_app/Scripts/Utils/Constants.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace STD.Utils
+{
+    public struct Constants
+    {
+        public struct Player
+        {
+            public const int MOVE_SPEED = 2;
+            public const float MAX_DEGREES = 720f;
+        }
+    }
+}
